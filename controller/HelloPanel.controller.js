7f5fb1a974bf99e62b6d69462fea100da2e1a8f1
N Fourtounis <nickfortune@windowslive.com>
2017-12-20 10:05:52 +0200

    Initial commit

---
+++ controller/HelloPanel.controller.js
@@ -0,0 +1,28 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+   "use strict";
+   return Controller.extend("sap.ui.demo.wt.controller.HelloPanel", {
+      onShowHello : function () {
+         // read msg from i18n model
+         var oBundle = this.getView().getModel("i18n").getResourceBundle();
+         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+         // show message
+         MessageToast.show(sMsg);
+      },
+      onShowBye : function () {
+         // read msg from i18n model
+         //ERROR
+         var oBundle = this.getView().getModel("i18n").getResourceBundle();
+         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+         var sMsg = oBundle.getText("ByeMsg", [sRecipient]);
+         // show message
+         MessageToast.show(sMsg);
+      },
+      onOpenDialog : function () {
+         this.getOwnerComponent().openHelloDialog();
+      }
+   });
+});
