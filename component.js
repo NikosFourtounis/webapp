5d9eb8c7ee15dc1d26c907e7828dcfad3950e8d6
N Fourtounis <nickfortune@windowslive.com>
2017-12-21 12:52:14 +0200

    clear

--- component.js
+++ component.js
@@ -1,31 +1,40 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/demo/wt/controller/HelloDialog",
-	"sap/ui/core/UIComponent"]
-], function (UIComponent, JSONModel, HelloDialog,UIComponent) {
+	"sap/ui/demo/wt/controller/HelloDialog"
+], function (UIComponent, JSONModel, HelloDialog) {
 	"use strict";
 
 	return UIComponent.extend("sap.ui.demo.wt.Component", {
 
-		metadata : {
-			manifest: "json",
-			rootView : "sap.viz.sample.Pie.Pie",
-            includes : ["../../css/exploredStyle.css"],
-            dependencies : {
-                libs : [
-                    "sap.viz",
-                    "sap.m"
-                ]
-            },
-            config : {
-                sample : {
-                    stretch : true,
-                    files : [
-                        "Pie.view.xml",
-                        "Pie.controller.js",
-                        "InitPage.js"
-                    ]
-                }
-            }
+		metadata: {
+			manifest: "json"
+		},
 
+		init: function () {
+
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// set data model
+			var oData = {
+				recipient: {
+					name: "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.setModel(oModel);
+
+			// set dialog
+			this._helloDialog = new HelloDialog(this.getRootControl());
+
+			// create the views based on the url/hash
+			this.getRouter().initialize();
+		},
+
+		openHelloDialog : function () {
+			this._helloDialog.open();
+		}
+	});
+
+});
